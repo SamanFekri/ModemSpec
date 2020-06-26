8b872e60affa2e4ceee286eb2c2f2ae9e01630bf
skings <samanf74@gmail.com>
2020-06-26 22:36:29 +0430

    An upgrade

--- Application/app/src/main/java/ir/irancell/application/ApiInterface.java
+++ Application/app/src/main/java/ir/irancell/application/ApiInterface.java
@@ -7,30 +7,38 @@ import retrofit2.Call;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.QueryMap;
 import retrofit2.http.Url;
 
 interface ApiInterface
 {
+    @Headers("User-Agent: SKings")
     @GET
     Call<ResponseBody> get(@Url String url);
 
+    @Headers("User-Agent: SKings")
     @GET
     Call<ResponseBody> get(@Url String url, @QueryMap HashMap<String, String> params);
 
+    @Headers("User-Agent: SKings")
     @GET
     Call<ResponseBody> get(@Url String url, @HeaderMap HashMap<String, String> headers, @QueryMap HashMap<String, String> params);
 
+    @Headers("User-Agent: SKings")
     @FormUrlEncoded
     @POST
     Call<ResponseBody> post(@Url String url);
 
+    @Headers("User-Agent: SKings")
     @FormUrlEncoded
     @POST
     Call<ResponseBody> post(@Url String url, @FieldMap HashMap<String, String> params);
 
+    @Headers("User-Agent: SKings")
     @FormUrlEncoded
     @POST
     Call<ResponseBody> post(@Url String url, @HeaderMap HashMap<String, String> headers, @FieldMap HashMap<String, String> params);
--- Application/app/src/main/java/ir/irancell/application/Constant.java
+++ Application/app/src/main/java/ir/irancell/application/Constant.java
@@ -2,8 +2,8 @@ package ir.irancell.application;
 
 public class Constant {
     //final
-    public static final String BASE_URL = "http://82.102.11.166:3444";
-    public static final String SPEED_TEST_URL = "http://speedtest.xfinity.com";
+    public static final String BASE_URL = "http://irancell.remarked.ir:3444";
+    public static final String SPEED_TEST_URL = "https://speedtest.xfinity.com/";
     public static final String MY_PREFS_NAME = "modem";
     public static final String MODEM_ID = "id";
 }
